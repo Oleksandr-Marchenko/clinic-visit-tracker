0bc2dc2c996c15f40b475d26fffe91399087ca33
Marchenko <aleks.marchenko802@gmail.com>
2025-09-16 15:01:25 +0300

    first commit

---
+++ src/main/java/com/marchenko/clinicvisittracker/ClinicVisitTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.marchenko.clinicvisittracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicVisitTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicVisitTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marchenko/clinicvisittracker/ClinicVisitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marchenko.clinicvisittracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicVisitTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
